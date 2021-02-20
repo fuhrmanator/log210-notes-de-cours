70de210a246b91d733f8833e21bdd72fa77c78e8
C. Fuhrman <christopher.fuhrman@etsmtl.ca>
2021-02-19 21:08:50 -0500

    Simpler pandoc plantuml filter

---
+++ src/filters/plantuml/pandoc_plantuml_filter.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+"""
+Pandoc filter to process code blocks with class "plantuml" into
+plant-generated images.
+Needs `plantuml.jar` from http://plantuml.com/.
+"""
+
+import os
+import sys
+import subprocess
+
+from pandocfilters import toJSONFilter, Para, Image
+from pandocfilters import get_filename4code, get_caption, get_extension
+
+PLANTUML_BIN = os.environ.get('PLANTUML_BIN', 'plantuml')
+
+
+def rel_mkdir_symlink(src, dest):
+    dest_dir = os.path.dirname(dest)
+    if not os.path.exists(dest_dir):
+        os.makedirs(dest_dir)
+
+    if os.path.exists(dest):
+        os.remove(dest)
+
+    src = os.path.relpath(src, dest_dir)
+    os.symlink(src, dest)
+
+
+def plantuml(key, value, format_, _):
+    if key == 'CodeBlock':
+        [[ident, classes, keyvals], code] = value
+
+        if "plantuml" in classes:
+            caption, typef, keyvals = get_caption(keyvals)
+
+            filename = get_filename4code("plantuml", code)
+            filetype = get_extension(format_, "png", html="svg", latex="pdf")
+
+            src = filename + '.uml'
+            dest = filename + '.' + filetype
+
+            # Generate image only once
+            if not os.path.isfile(dest):
+                txt = code.encode(sys.getfilesystemencoding())
+                if not txt.startswith(b"@start"):
+                    txt = b"@startuml\n" + txt + b"\n@enduml\n"
+                with open(src, "wb") as f:
+                    f.write(txt)
+
+                # The PDF modules have log messages to stdout that corrupt the JSON stream
+                with open('plantUMLErrors.log', "a+") as log_file:
+                    subprocess.check_call(PLANTUML_BIN.split() +
+                                      ["-t" + filetype, src], stdout=log_file, stderr=log_file)
+
+                sys.stderr.write('Created image ' + dest + '\n')
+
+            # Update symlink each run
+            for ind, keyval in enumerate(keyvals):
+                if keyval[0] == 'plantuml-filename':
+                    link = keyval[1]
+                    keyvals.pop(ind)
+                    rel_mkdir_symlink(dest, link)
+                    dest = link
+                    break
+
+            return Para([Image([ident, [], keyvals], caption, [dest, typef])])
+
+
+def main():
+    toJSONFilter(plantuml)
+
+
+if __name__ == "__main__":
+    main()
